a52092ca398a73830a516aa58e2ce07eae66485d
blanpa <pascalblansche@gmail.com>
2023-02-03 21:12:36 +0000

    fix

--- week_2_workflow_orchestration/flows/01_start/ingest_data_self.py
+++ week_2_workflow_orchestration/flows/01_start/ingest_data_self.py
@@ -3,25 +3,22 @@
 import os
 import argparse
 from time import time
-import pandas as pd
+import pandas as pdconda 
 from sqlalchemy import create_engine
-
-# prefect
 from prefect import flow, task
 from prefect.tasks import task_input_hash
 from datetime import timedelta
 from prefect_sqlalchemy import SqlAlchemyConnector
 
-
-@task(log_prints = True, tags = ["extract"], cache_key_fn = task_input_hash, cache_expiration = timedelta(days=1))
+@task(log_prints=True, retries = 3, cache_key_fn =task_input_hash, cache_expiration = timedelta(days=1))
 def extract_data(url: str):
-    # the backup files are gzipped, and it's important to keep the correct extension
+   # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
         csv_name = 'yellow_tripdata_2021-01.csv.gz'
     else:
         csv_name = 'output.csv'
-    
+
     os.system(f"wget {url} -O {csv_name}")
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
@@ -41,25 +38,32 @@ def transform_data(df):
     return df
 
 @task(log_prints=True, retries=3)
-def load_data(table_name, df):
-    
-    connection_block = SqlAlchemyConnector.load("postgres-connector")
+def ingest_data(table_name, df):
+    connection_block =  SqlAlchemyConnector.load("sql")
     with connection_block.get_connection(begin=False) as engine:
+        # postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+        # engine = create_engine(postgres_url)
         df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
         df.to_sql(name=table_name, con=engine, if_exists='append')
 
-@flow(name="Subflow", log_prints=True)
+
+@flow(name = "SUbflow", log_prints=True)
 def log_subflow(table_name: str):
-    print(f"Logging Subflow for: {table_name}")
+    print(f"loggin Subflow for : {table_name}")
 
-@flow(name="Ingest Data")
-def main_flow(table_name: str = "yellow_taxi_trips"):
-
+@flow(name="Ingest Flow")
+def main_flow(table_name: str):
+    user = "root"
+    password = "root"
+    host = "localhost"
+    port = "5432"
+    db = "ny_taxi"
+    table_name = table_name
     csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
     log_subflow(table_name)
     raw_data = extract_data(csv_url)
     data = transform_data(raw_data)
-    load_data(table_name, data)
+    ingest_data(table_name, data)
 
 if __name__ == '__main__':
-    main_flow(table_name = "yellow_trips")
+    main_flow("yellow_taxi_trips")
