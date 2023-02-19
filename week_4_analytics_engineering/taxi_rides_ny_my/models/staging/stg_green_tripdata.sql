6b4783423ccd6f2fc902b47b3803fb31044a99b2
blanpa <pascalblansche@gmail.com>
2023-02-19 18:14:40 +0000

    test

--- week_4_analytics_engineering/taxi_rides_ny_my/models/staging/stg_green_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny_my/models/staging/stg_green_tripdata.sql
@@ -1,8 +1,6 @@
-{{ 
-    config(materialized='view') 
-}}
+{{ config(materialized="view") }}
 
 -- utility-logic-375619.trips_data_all.yellow_tripdata
-
-select * FROM {{ source('staging', 'green_tripdata')}}
+select *
+from {{ sources("staging", "green_tripdata") }}
 limit 100
